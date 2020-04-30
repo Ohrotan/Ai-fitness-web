c80ce51b934a8ee961364814da7c70b2d26af2e7
Ohrotan <ygj02054@gmail.com>
2020-04-30 23:37:32 +0900

    mem create

--- src/member/create.php
+++ src/member/create.php
@@ -2,10 +2,10 @@
 <?php
 include "dbconfig.php";
 
-$name = $_GET[name]; //$_POST[name];
-$id = $_GET[id];
+$name = $_POST[name]; //$_POST[name];
+$id = $_POST[id];
 
-$sql = "INSERT INTO member (id, name) VALUES ('$name','$id')";
+$sql = "INSERT INTO member (id, name) VALUES ('$id','$name')";
 
 $result = mysqli_query($con, $sql);
 mysqli_close($con);
