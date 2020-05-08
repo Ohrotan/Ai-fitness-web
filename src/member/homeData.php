2d04dbde373902dba7c589009d9083166b05e92e
GwanUk Han <rhksdnr48@naver.com>
2020-05-08 20:27:10 +0900

    수정:homeData.php 추가:

--- src/member/homeData.php
+++ src/member/homeData.php
@@ -10,22 +10,24 @@ $result_array = array();
 
 
 if ($trainer === 1) { //요청 유저가 트레이너라면 트레이너가 등록한 프로그램 정보를 전송함.
-
     $results = $db->query("select name, title from trainer as t join exr_program as e on t.id = e.trainer_id where t.id = '$user_id' limit 3");
-
-    array_push($result_array, $results->rowCount());//쿼리 결과 row 개수를 같이 전송함
-
-    if ($results->rowCount() > 0)
-    {
-        foreach ($results as $row){
-            array_push($result_array,$row);
-        }
-
-    }
 }
 else {//트레이너가 아니라면 신청한 프로그램 정보를 전송함.
+    //*****여기 쿼리 부분 수정해야함
+    $results = $db->query("select name, title from trainer as t join exr_program as e on t.id = e.trainer_id where t.id = '$user_id' limit 3");
+}
+
+array_push($result_array, $results->rowCount());//쿼리 결과 row 개수를 같이 전송함
+
+if ($results->rowCount() > 0)
+{
+    foreach ($results as $row){
+        array_push($result_array,$row);
+    }
 
 }
+
+
 //*****트레이너 목록 추가
 $results = $db->query("select id, name, image from trainer limit 4");
 
