9c009ff644699c40e5f64f7e7dd461183f7d8862
Ohrotan <ygj02054@gmail.com>
2020-05-08 19:37:56 +0900

    (done) video upload

--- src/video/tr_video_create.php
+++ src/video/tr_video_create.php
@@ -18,22 +18,23 @@ $sql = "INSERT INTO trainer_video (trainer_id, thumb_img, video, title) VALUES (
 $allowedExts = array("jpg", "jpeg", "gif", "png", "mp3", "mp4", "wma");
 $extension = pathinfo($_FILES['myFile']['name'], PATHINFO_EXTENSION);
 $video_file_name =  "1".$_FILES["myFile"]["name"];
-echo $_FILES["myFile"]["type"]."  ";
-if ((($_FILES["myFile"]["type"] == "video/mp4")
+
+if (true||(($_FILES["myFile"]["type"] == "video/mp4")
         || ($_FILES["myFile"]["type"] == "audio/mp3")
         || ($_FILES["myFile"]["type"] == "audio/wma")
         || ($_FILES["myFile"]["type"] == "image/pjpeg")
         || ($_FILES["myFile"]["type"] == "image/gif")
         || ($_FILES["myFile"]["type"] == "image/jpeg"))
-    && ($_FILES["myFile"]["size"] < 600000000)) {
+    && ($_FILES["myFile"]["size"] < 100*1024*1024)) {
     if ($_FILES["myFile"]["error"] > 0) {
+
         echo "Return Code: " . $_FILES["myFile"]["error"] . "<br />";
     } else {
         echo "Upload: " . $video_file_name. "<br />";
         echo "Type: " . $_FILES["myFile"]["type"] . "<br />";
         echo "Size: " . ($_FILES["myFile"]["size"] / 1024) . " Kb<br />";
         echo "Temp file: " . $_FILES["myFile"]["tmp_name"] . "<br />";
-
+        echo "  37line ";
         //echo file_get_contents( $_FILES["myFile"]["tmp_name"])."<br>";
     //   $f =  fopen($_FILES["myFile"]["tmp_name"], 'w+');
      //  fwrite($f,"aa");
@@ -94,23 +95,3 @@ if ((($_FILES["myFile"]["type"] == "video/mp4")
 } else {
     echo "Invalid file";
 }
-/*
-
-if($_SERVER['REQUEST_METHOD']=='POST'){
-
-    $file_name = $_FILES['myFile']['name'];
-    $file_size = $_FILES['myFile']['size'];
-    $file_type = $_FILES['myFile']['type'];
-    $temp_name = $_FILES['myFile']['tmp_name'];
-
-    $location = "/srv/static/video/";
-
-    move_uploaded_file($temp_name, $location.$file_name);
-    echo "/srv/static/video/".$file_name."<br>";
-
-
-
-}else{
-    echo "Error";
-}
-*/
