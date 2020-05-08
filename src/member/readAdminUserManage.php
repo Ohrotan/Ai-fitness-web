81bb78c25dcb0b6504ac74bba858c637ad3880d7
Jeho Ryu <jhryu951029@gmail.com>
2020-05-09 05:50:32 +0900

    관리자 계정 전체회원목록 조회 수정

--- src/member/readAdminUserManage.php
+++ src/member/readAdminUserManage.php
@@ -2,20 +2,25 @@
 
 include "dbconfig.php";
 
-$name = $_GET['name'];
-$trainer = $_GET['trainer'];
+$name = $_GET[name];
+$trainer = $_GET[trainer];
 
 //$sql = "select name, trainer from member";
 
 $results = $db->query('SELECT name, trainer FROM member');
 $result_array = array();
+
+//var_dump($results->rowCount());
+echo("<br>");
+array_push($result_array, $results->rowCount());
+
 if ($results->rowCount() > 0)
 {
     foreach ($results as $row){
+        //echo $row[name]."/".$row[trainer]."<br>";
         array_push($result_array,$row);
     }
 
 }
-
-echo json_encode(array("result"=>$result_array));
+echo json_encode($result_array);
 ?>
