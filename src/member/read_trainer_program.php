82e573ec04b976e6902d51147c3e595724a547cc
Jeho Ryu <jhryu951029@gmail.com>
2020-05-15 00:07:19 +0900

    트레이너 프로필 조회 수정

--- src/member/read_trainer_program.php
+++ src/member/read_trainer_program.php
@@ -1,24 +1,46 @@
 <?php
 
-/*include "dbconfig.php";
+include "dbconfig.php";
 
-$trainer_id = $_POST[$trainer_id];
+$exr_id = $_POST[exr_id];
 //$sql = "select name, trainer from member";
 
-$results = $db->query("SELECT id, title, period, equip, gender, level, max FROM exr_program WHERE trainer_id = '$trainer_id'");
+//기본정보
+$results0 = $db->query("SELECT title, period, equip, gender, level, max FROM exr_program WHERE id = '$exr_id'");
 
-$results2 = $db->query("SELECT count(MP.mem_id) AS curMemberNum
+//현재 회원 수
+$results1 = $db->query("SELECT count(MP.mem_id) AS cur_member_num 
 FROM member_reg_program MP join exr_program EP 
-WHERE '$trainer_id' = MP.exr_id AND MP.end_date > CURDATE()
-GROUP BY '$trainer_id'");
+WHERE EP.id = '$exr_id' AND MP.exr_id = '$exr_id' AND MP.end_date > CURDATE()
+GROUP BY EP.id");
+
+//누적 회원 수
+$results2 = $db->query("SELECT count(MP.mem_id) AS total_member_num 
+FROM member_reg_program MP join exr_program EP 
+WHERE EP.id = '$exr_id' and MP.exr_id = '$exr_id'
+GROUP BY EP.id");
+
+//누적 평점
+$results3 = $db->query("SELECT avg(rating) as avg_rating
+FROM member_reg_program MP join exr_program EP 
+WHERE EP.id = '$exr_id' AND MP.exr_id = '$exr_id'
+GROUP BY EP.id");
 $result_array = array();
 //var_dump($results->rowCount());
 //echo("<br>");
 //array_push($result_array, $results->rowCount());
 
-if ($results->rowCount() > 0)
+if ($results0->rowCount() > 0)
 {
-    foreach ($results as $row){
+    foreach ($results0 as $row){
+        //echo $row[name]."/".$row[trainer]."<br>";
+        array_push($result_array,$row);
+    }
+}
+
+if ($results1->rowCount() > 0)
+{
+    foreach ($results1 as $row){
         //echo $row[name]."/".$row[trainer]."<br>";
         array_push($result_array,$row);
     }
@@ -31,4 +53,12 @@ if ($results2->rowCount() > 0)
         array_push($result_array,$row);
     }
 }
-echo json_encode($result_array);*/
+
+if ($results3->rowCount() > 0)
+{
+    foreach ($results3 as $row){
+        //echo $row[name]."/".$row[trainer]."<br>";
+        array_push($result_array,$row);
+    }
+}
+echo json_encode($result_array);
