a8a3bed14a8479770fd7ee7a1c005275049e224f
Ohrotan <ygj02054@gmail.com>
2020-05-14 12:21:40 +0900

    exr crud forms in home.html

--- src/exr/create_exr_program.php
+++ src/exr/create_exr_program.php
@@ -4,14 +4,15 @@ include "dbconfig.php";
 $trainer_id = $_POST[trainer_id];
 $title = $_POST[title];
 $period = $_POST[period];
+$equip = $_POST[equip];
 $gender = $_POST[gender];
 $level = $_POST[level];
 $max = $_POST[max];
 $intro = $_POST[intro];
 
 //1번 방법
-$sql = "INSERT INTO exr_program (trainer_id, title, period, gender, level, max, intro) "
-    . "VALUES ('$trainer_id','$title','$period','$gender','$level','$max','$intro')";
+$sql = "INSERT INTO exr_program (trainer_id, title, period, equip, gender, level, max, intro) "
+    . "VALUES ('$trainer_id','$title','$period','$equip','$gender','$level','$max','$intro')";
 $stmt = $db->prepare($sql);
 $result = $stmt->execute();
 echo $result;
