879c9f8925b708384ba7c032ecbe96aa883fa544
Jeho Ryu <jhryu951029@gmail.com>
2020-05-16 21:13:23 +0900

    프로그램 등록한 회원 목록 수정

--- src/member/read_reg_member.php
+++ src/member/read_reg_member.php
@@ -5,9 +5,9 @@ include "dbconfig.php";
 $exr_id = $_POST[exr_id];
 //$sql = "select name, trainer from member";
 
-$results = $db->query("SELECT M.name
-FROM member M join member_reg_program MP
-WHERE M.id = MP.mem_id and MP.end_date > CURDATE() and MP.exr_id = '$exr_id'");
+$results = $db->query("SELECT M.name, EP.title
+FROM member M join member_reg_program MP join exr_program EP
+WHERE M.id = MP.mem_id and MP.end_date > CURDATE() and EP.id = '$exr_id' and MP.exr_id = '$exr_id'");
 
 //$results2 = $db->query("select memberNum from trainer_member_num TM join member M where TM.trainer_id = M.id and name = '$name'");
 $result_array = array();
