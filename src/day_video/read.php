bf8354cf7b3fa937b44280e9290245a2567923b9
Ohrotan <ygj02054@gmail.com>
2020-05-25 11:00:44 +0900

    day_program_video read.php query

--- src/day_video/read.php
+++ src/day_video/read.php
@@ -6,11 +6,11 @@ $id = $_POST['id'];
 $day_id = $_POST['day_id'];
 
 if ($id != null) {
-    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video WHERE id = '$id'");
+    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video ON day_program_video.video_id = trainer_video.id WHERE id = '$id'");
 } else if ($day_id != null) {
-    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video WHERE day_id = '$day_id' ORDER BY seq");
+    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video ON day_program_video.video_id = trainer_video.id WHERE day_id = '$day_id' ORDER BY seq");
 } else {
-    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video");
+    $results = $db->query("SELECT day_program_video.*, title FROM day_program_video JOIN trainer_video ON day_program_video.video_id = trainer_video.id");
 }
 
 $result_array = array();
