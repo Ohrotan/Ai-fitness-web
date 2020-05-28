796dcda1636582f844efef315af94acabe7a0503
jhryu10 <55955300+jhryu10@users.noreply.github.com>
2020-05-29 01:12:52 +0900

    read_analysis 추가

---
+++ src/member/read_analysis.php
@@ -0,0 +1,22 @@
+<?php
+
+include "dbconfig.php";
+
+$day_id = $_POST['day_id'];
+
+$results = $db->query("SELECT analysis
+FROM trainer_video
+WHERE id IN (SELECT video_id
+             FROM day_program_video
+             WHERE day_id = '$day_id');");
+$result_array = array();
+
+if ($results->rowCount() > 0)
+{
+    foreach ($results as $row){
+        array_push($result_array,$row);
+    }
+
+}
+
+echo json_encode(array($result_array));
