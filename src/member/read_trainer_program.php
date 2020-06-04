62224d6b053e58575d5322fd9c847b4300275f21
jhryu10 <55955300+jhryu10@users.noreply.github.com>
2020-06-05 02:07:03 +0900

    read_trainer_program.php 수정

--- src/member/read_trainer_program.php
+++ src/member/read_trainer_program.php
@@ -12,16 +12,14 @@ $results0 = $db->query("SELECT title, period, equip, gender, level, max FROM exr
 $results1 = $db->query("select curMemberNum from program_cur_member_num where id = '$exr_id'");
 
 //누적 회원 수
-$results2 = $db->query("SELECT count(MP.mem_id) AS total_member_num 
-FROM member_reg_program MP join exr_program EP 
-WHERE EP.id = '$exr_id' and MP.exr_id = '$exr_id'
-GROUP BY EP.id");
+$results2 = $db->query("SELECT total_member_num
+FROM program_total_member_num
+WHERE id = '$exr_id'");
 
 //누적 평점
-$results3 = $db->query("SELECT avg(rating) as avg_rating
-FROM member_reg_program MP join exr_program EP 
-WHERE EP.id = '$exr_id' AND MP.exr_id = '$exr_id'
-GROUP BY EP.id");
+$results3 = $db->query("SELECT avg_rating
+FROM program_total_rating
+WHERE id = '$exr_id'");
 $result_array = array();
 //var_dump($results->rowCount());
 //echo("<br>");
