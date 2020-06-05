4b76d011b208137dbf4e53bf9610d3282ffd00ec
ChoiSeungHyuk <wgpoi@naver.com>
2020-06-05 14:12:31 +0900

    상세정보 쿼리 수정

--- src/member/member_exr_program.php
+++ src/member/member_exr_program.php
@@ -5,7 +5,7 @@ include "dbconfig.php";
 $id = $_POST['mem_id']; //$_POST[name];
 
 
-$results = $db->query("SELECT e.id as exr_id, m2.name, title, start_date, end_date, time, mem_cnt, dp.day_id, day_title, day_intro from member as m1, member as m2, exr_program as e, member_reg_program as mrp left JOIN(SELECT * FROM member_exr_history)as meh ON mrp.exr_id = meh.exr_id join mem_cnt_view left join (select exr_id,id as day_id, title as day_title, intro as day_intro from day_program) as dp on dp.exr_id = mrp.exr_id where mrp.mem_id = '$id' and m1.id = mrp.mem_id and mrp.exr_id = e.id and e.trainer_id = m2.id and mem_cnt_view.exr_id = e.id;");
+$results = $db->query("SELECT e.id as exr_id, m2.name, title, start_date, end_date, time, mem_cnt, dp.day_id, day_title, day_intro from member as m1, member as m2, exr_program as e, member_reg_program as mrp left JOIN(SELECT * FROM member_exr_history)as meh ON mrp.exr_id = meh.exr_id join mem_cnt_view left join (select exr_id,id as day_id, title as day_title, intro as day_intro from day_program) as dp on dp.exr_id = mrp.exr_id where mrp.mem_id = '$id' and m1.id = mrp.mem_id and mrp.exr_id = e.id and e.trainer_id = m2.id and mem_cnt_view.exr_id = e.id");
 $result_array = array();
 if ($results->rowCount() > 0)
 {
