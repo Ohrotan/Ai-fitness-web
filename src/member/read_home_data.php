c92d406c992673b4ee4cb8d5b90af9c0f0ae1c83
GwanUk Han <rhksdnr48@naver.com>
2020-06-13 16:43:12 +0900

    수정: read-home_data

--- src/member/read_home_data.php
+++ src/member/read_home_data.php
@@ -9,12 +9,11 @@ $trainer = (int)$_POST['trainer'];
 $result_array = array();
 
 
-if ($trainer === 1) { //요청 유저가 트레이너라면 트레이너가 등록한 프로그램 정보를 전송함.
-    $results = $db->query("select t.id, name, title from trainer as t join exr_program as e on t.id = e.trainer_id where t.id = '$user_id' limit 3");
+if ($trainer === 1) { //요청 유저가 트레이너라면 트레이너가 등록한 프로그램 중에서 피드백 대기중인 정보를 전송함.
+    $results = $db->query("select e.id, name, title from trainer as t join exr_program as e on t.id = e.trainer_id where t.id = '$user_id' limit 3");
 }
 else {//트레이너가 아니라면 신청한 프로그램 정보를 전송함.
-    //*****여기 쿼리 부분 수정해야함
-    $results = $db->query("select t.id, t.name, e.title from member_reg_program m join (trainer t join exr_program e on t.id = e.trainer_id) on e.id = m.exr_id  where m.mem_id = '$user_id' limit 3");
+    $results = $db->query("select e.id, t.name, e.title from member_reg_program m join (trainer t join exr_program e on t.id = e.trainer_id) on e.id = m.exr_id  where m.mem_id = '$user_id' limit 3");
 }
 
 array_push($result_array, $results->rowCount());//쿼리 결과 row 개수를 같이 전송함
