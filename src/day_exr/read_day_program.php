d9c35408960822ee3bb1dbcfd04451bf9996aada
Ohrotan <ygj02054@gmail.com>
2020-06-15 15:51:19 +0900

    read_day_program.php select *

--- src/day_exr/read_day_program.php
+++ src/day_exr/read_day_program.php
@@ -6,14 +6,14 @@ $day_program_id = (int)$_POST['day_program_id'];
 $result_array = array();
 
 //day_program의 intro, title 얻기
-$results = $db->query("select intro, title from day_program where id = '$day_program_id'");
+$results = $db->query("select * from day_program where id = '$day_program_id'");
 array_push($result_array, $results->rowCount());
 if ($results->rowCount() > 0) {
     foreach ($results as $row) {
         array_push($result_array, $row);
     }
 
-    $results = $db->query("select counts, sets, thumb_img, video, title from day_program_video dpv join trainer_video tv on dpv.video_id = tv.id where day_id = '$day_program_id' order by seq");
+    $results = $db->query("select * from day_program_video dpv join trainer_video tv on dpv.video_id = tv.id where day_id = '$day_program_id' order by seq");
     array_push($result_array, $results->rowCount());
     if ($results->rowCount() > 0) {
         foreach ($results as $row) {
